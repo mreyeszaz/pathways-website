a5cf0c79e1eb057906bf4fccedfffef7a35991b4
Uwem Ekong <uweme6@gmail.com>
2020-07-29 01:21:37 -0500

    Added proxy for frontend to talk to backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 
-
-
-
 function App() {
-  return <h1>Pathways</h1>;
+  // React hook that allows us to perform side effets in our App component
+  useEffect(() => {
+    fetch("/api/pathways").then((response) =>
+      response.json().then((data) => {
+        console.log(data);
+      })
+    );
+  }, []);
+  return <div className="App" />;
 }
-
 
 export default App;
